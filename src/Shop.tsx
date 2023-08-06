06ddc255c45965ec2aa9c630649afded3808f427
Michael-Crowther <67437978+Michael-Crowther@users.noreply.github.com>
2023-08-05 18:16:22 -0600

    Add files via upload

---
+++ src/Shop.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shop: React.FC = () => {
+    return (
+        <section id="Shop">
+            <h2>Shop</h2>
+        </section>
+    );
+}
+
+export default Shop;

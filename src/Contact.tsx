28caa7086f4996f6498583e84f6df53fd8088b3e
Michael-Crowther <67437978+Michael-Crowther@users.noreply.github.com>
2023-08-13 10:38:20 -0600

    Add files via upload

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -21,7 +21,7 @@ const ContactForm: React.FC = () => {
         });
     }
 
-    /*
+    
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         
         setErrorMessage('');
@@ -43,14 +43,14 @@ const ContactForm: React.FC = () => {
                 email: '',
                 message: '',
             });
-            setIsModalOpen(true);
+            //setIsModalOpen(true);
             document.documentElement.classList.add('modal-open');
         }   
     }
-    */
+    
     
 
-    
+    /*
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
         e.preventDefault();
     
@@ -98,6 +98,7 @@ const ContactForm: React.FC = () => {
             setErrorMessage('An error occurred. Please try again.');
         }
     };
+    */
     
     
 

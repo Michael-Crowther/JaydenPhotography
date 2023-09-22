ab6f34aec30d9e74b231140a76dd0c6a9d0120db
Michael Crowther <md.crowther@yahoo.com>
2023-09-22 10:36:52 -0600

    added test comment to App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,194 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import Gallery from './Gallery';
+import Shop from './Shop';
+import Contact from './Contact';
+import image from './images/headshot.png';
+
 
 function App() {
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+  //UseEffect hook for checking if user scrolls. Change navbar if user scrolls
+  useEffect(() => {
+    const header = document.getElementById('Top') as HTMLElement;
+    const logo = document.getElementById('logo') as HTMLElement;
+    const navbarTextElements = document.getElementsByClassName('navbarText') as HTMLCollectionOf<HTMLElement>;
+    const icon = document.getElementById('icon') as HTMLElement;
+
+    const navbarTextArray = Array.from(navbarTextElements); // Convert HTMLCollection to an array
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const threshold = 25; // Adjust this value as needed
+
+      if (scrollPosition > threshold) {
+        header.classList.add("stickyTop");
+        logo.classList.add("stickyLogo");
+        icon.classList.add("stickyLogo");
+        header.classList.remove("stickyFadeOut");
+        navbarTextArray.forEach((element) => {//Gets all text elements in navbar
+          element.classList.add('stickyNavBarText');
+        });
+
+      } 
+      else {
+        if (scrollPosition <= threshold && scrollPosition === 0) {
+          header.classList.add("stickyFadeOut");
+          setTimeout(() => {//Wait for fadeOut animtation to end
+            header.classList.remove("stickyTop");
+            logo.classList.remove("stickyLogo");
+            icon.classList.remove("stickyLogo");
+            navbarTextArray.forEach((element) => {
+              element.classList.remove('stickyNavBarText');
+            });   
+          }, 200);
+        }
+      } 
+    };
+
+    // Add event listener on component mount
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  //UseEffect hook to handle resetting page when user refreshes
+  useEffect(() => {
+    const handlePageRefresh = () => {
+      // Scroll to the top of the page when the user refreshes it
+      window.scrollTo(0, 0);
+    };
+    window.addEventListener("beforeunload", handlePageRefresh);
+
+    return () => {
+      window.removeEventListener("beforeunload", handlePageRefresh);
+    };
+  }, []);
+
+  //This effect will prevent having both nav bars showing
+  useEffect(() => {
+
+    const hamburgerIcon = document.getElementById("icon");
+
+    function handleResize(){
+      
+      if(window.innerWidth >= 1400){
+        setIsMobileNavOpen(false);
+        hamburgerIcon?.classList.remove("visible");
+      }
+      else if(hamburgerIcon?.classList.contains("hidden")){
+        setIsMobileNavOpen(true);
+      }
+      
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isMobileNavOpen]);
+
+  //This effect checks if the user clicks anywhere outside of the mobile nav bar
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent){
+      const targetElement = event.target as Element;
+      const hamburgerIcon = document.getElementById("icon");
+
+      //Close navbar and make bars visible if the user clicks a class outside
+      //of the mobileLinks and if the mobile navbar is visible
+      if(!targetElement.closest("#mobileLinks") && hamburgerIcon?.classList.contains("hidden")){
+        setIsMobileNavOpen(false);
+        hamburgerIcon.classList.toggle("hidden");
+      }
+      
+    }
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    }
+  }, []);
+
+  const handleMobileNavbarClick = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+
+    const hamburgerIcon = document.getElementById("icon");
+    //const mobileNavBar = document.getElementById("mobileLinks");
+
+
+    if(hamburgerIcon?.classList.contains("hidden")){
+      //If hidden, make it visible instantly
+      hamburgerIcon.classList.remove("hidden");
+      hamburgerIcon.classList.add("visible");
+      //mobileNavBar?.classList.add("hidden");
+    }
+    else{
+      //If visible, hide after a delay
+      hamburgerIcon?.classList.remove("visible");
+      setTimeout(() => {
+        hamburgerIcon?.classList.add("hidden");
+        //mobileNavBar?.classList.remove("hidden");
+      }, 300);
+    }   
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App" id="linkTop">
+
+      <header id="Top">
+        <div className="logo" id="logo">Jayden Crowther</div>
+        <nav className="navbar" id="navbar">
+          <FontAwesomeIcon icon={faBars} id="icon" className="icon" onClick={handleMobileNavbarClick} />
+          <ul id="myLinks">
+            <li><a className="navbarText" href="#linkTop">Home</a></li>
+            <li><a className="navbarText" href="#About">About</a></li>
+            <li><a className="navbarText" href="#Gallery">Gallery</a></li>
+            <li><a className="navbarText" href="#Shop">Shop</a></li>
+            <li><a className="navbarText" href="#Contact">Contact</a></li>
+          </ul>
+        </nav>
       </header>
-    </div>
+
+      <ul id="mobileLinks" className={isMobileNavOpen ? 'open' : ' '}>        
+        <li><a className="navbarText" href="#linkTop" onClick={handleMobileNavbarClick}>Home</a></li>
+        <li><a className="navbarText" href="#About" onClick={handleMobileNavbarClick}>About</a></li>
+        <li><a className="navbarText" href="#Gallery" onClick={handleMobileNavbarClick}>Gallery</a></li>
+        <li><a className="navbarText" href="#Shop" onClick={handleMobileNavbarClick}>Shop</a></li>
+        <li><a className="navbarText" href="#Contact" onClick={handleMobileNavbarClick}>Contact</a></li>
+        <FontAwesomeIcon icon={faBars} id="mobileIcon" onClick={handleMobileNavbarClick} />
+      </ul>
+
+
+      <section id="Home">
+        <h1 className="homeHeader">Photography</h1>
+        <h2 className="homeSubHeader">at its finest.</h2>
+      </section>
+
+      <section id="About">
+        <div className="about-parent">
+          <img src={image} alt="profile" className="about-image" />
+          <div className="about-content">
+            <h3 className="aboutHeader">Hey, I'm Jayden</h3>
+            <p className="aboutParagraph">I'm a photographer from the beautiful Utah Valley. People say I have a knack for making them feel at ease in front of the camera. Whether it's capturing candid moments, vibrant portraits, or those once-in-a-lifetime family memories, my goal is to make every shot count and give you photos you'll cherish. Feel free to contact me so we can schedule a shoot!</p>
+          </div>
+        </div>
+      </section>
+
+      <Gallery/>
+
+      <Shop/>
+
+      <Contact/>
+  </div>
   );
 }
 
---
+++ src/Gallery.tsx
@@ -0,0 +1,164 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowLeft, faTimes } from '@fortawesome/free-solid-svg-icons';
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
+import img4 from "./images/img4.jpg";
+import img5 from "./images/img5.jpg";
+import img6 from "./images/img6.jpg";
+import img7 from "./images/img7.jpg";
+import img8 from "./images/img8.jpg";
+import img9 from "./images/img9.jpg";
+import img10 from "./images/img10.jpg";
+import img11 from "./images/img11.jpg";
+import img12 from "./images/img12.jpg";
+
+
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12]
+
+/**Start Functional Component */
+const Gallery: React.FC = () => {
+
+  const [showModal, setShowModal] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isPortraitImage, setIsPortraitImage] = useState(false);
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [isContentVisible, setIsContentVisible] = useState(true);
+
+  const imageLoadedRef = useRef(imageLoaded);
+  const modalContentRef = useRef<HTMLDivElement>(null);
+  const touchStartX = useRef<number>(0);//for mobile swipe on gallery
+  const [isLoading, setIsLoading] = useState(false);
+
+
+      /**Gallery Section Functions */
+
+  const goToLeftImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    setImageLoaded(false);
+    setIsContentVisible(false); // <- hide content initially
+    loadTimer();
+  }
+
+  const loadTimer = () => {
+    setTimeout(() => {
+        if(imageLoadedRef.current === false)
+            setIsLoading(true);
+        setIsContentVisible(true);
+    }, 300);
+  }
+
+  const goToRightImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    setImageLoaded(false);
+    setIsContentVisible(false); // <- hide content initially
+    loadTimer();   
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setIsPortraitImage(false);
+    setIsLoading(false);
+
+    document.documentElement.classList.remove('modal-open');
+
+    const header = document.getElementById("Top");
+
+    header?.classList.toggle("hidden");
+  }
+
+  const openModal = () => {
+    setShowModal(true);
+    setIsContentVisible(false); // <- hide content initially
+    setImageLoaded(false);
+    loadTimer();
+
+    document.documentElement.classList.add('modal-open');
+
+    const header = document.getElementById("Top");
+
+    header?.classList.toggle("hidden");
+  }
+
+  // Ensure ref is always in sync with state
+    useEffect(() => {
+        imageLoadedRef.current = imageLoaded;
+    }, [imageLoaded]);
+
+  const handleImageLoad = () => {
+    const modalContent = modalContentRef.current;
+    setIsLoading(false);
+    setImageLoaded(true);
+    setIsContentVisible(true);  // <- make content visible when image is loaded
+    console.log("image loaded");
+
+    if(modalContent){
+      const rect = modalContent.getBoundingClientRect();
+      const height = rect.height;
+
+      modalContent?.classList.remove("hidden");
+
+      if(height && height > 550){
+        setIsPortraitImage(true);
+      }
+      else{
+        setIsPortraitImage(false);
+      }
+    }
+
+  }
+
+  /**Mobile Functions on Gallery */
+  const handleTouchStart = (e: React.TouchEvent<HTMLImageElement>) => {
+    touchStartX.current = e.touches[0].clientX;
+  }
+
+  //Swipe functionality on Gallery on mobile
+  const handleTouchMove = (e: React.TouchEvent<HTMLImageElement>) => {
+    if(!touchStartX.current){
+      return;
+    }
+    const touchEndX = e.touches[0].clientX;
+    const swipeLength = touchStartX.current - touchEndX;
+
+    if(Math.abs(swipeLength) > 100){
+      if(swipeLength > 0){
+        goToRightImage();
+      }
+      else{
+        goToLeftImage();
+      }
+    }
+  }
+
+    return (
+        <section id="Gallery">
+        <div className="image-container">
+          <FontAwesomeIcon icon={faArrowLeft} className="arrow left-arrow" onClick={goToLeftImage} />
+          <img src={images[currentImageIndex]} alt="gallery" className="gallery-image" onClick={openModal} />
+          <FontAwesomeIcon icon={faArrowRight} className="arrow right-arrow" onClick={goToRightImage} />
+        </div>
+        {showModal &&
+          <div className="modal" onClick={closeModal}>
+            <div id="modal-content" 
+            ref={modalContentRef} 
+            key={currentImageIndex} 
+            className={`modal-content ${!isContentVisible || isLoading ? 'hidden-modal-content' : ''} ${isPortraitImage ? 'portrait' : ''}`} 
+            onClick={e => e.stopPropagation()} 
+            onTouchStart={handleTouchStart} 
+            onTouchMove={handleTouchMove}>
+                <FontAwesomeIcon icon={faTimes} className="close-icon" onClick={closeModal}/>
+                <FontAwesomeIcon icon={faArrowLeft} className="arrow left-arrow-modal" onClick={goToLeftImage} />
+                <img src={images[currentImageIndex]} alt="gallery" id="modal-image" className="modal-image" onLoad={handleImageLoad} />
+                <FontAwesomeIcon icon={faArrowRight} className="arrow right-arrow-modal" onClick={goToRightImage} />
+            </div>
+
+            {isLoading && <div className="loading" id="loading"></div>}
+          </div>
+        }
+      </section>
+    );
+}
+
+export default Gallery;
